7ca05ad4ddbfbb3804ddbe742a466586005f6172
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-11-15 22:04:43 +0200

    add header logout button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,26 +4,34 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const isAuthed = true;
+
   return (
     <header>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#">Tinder for ducks</Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link
-              to="/login"
-              as={Link}
-              active={window.location.pathname === "/login"}
-            >
-              Login
-            </Nav.Link>
-            <Nav.Link
-              to="/register"
-              as={Link}
-              active={window.location.pathname === "/register"}
-            >
-              Sign Up
-            </Nav.Link>
+            {isAuthed ? (
+              <Nav.Link as="button">Logout</Nav.Link>
+            ) : (
+              <>
+                <Nav.Link
+                  to="/login"
+                  as={Link}
+                  active={window.location.pathname === "/login"}
+                >
+                  Login
+                </Nav.Link>
+                <Nav.Link
+                  to="/register"
+                  as={Link}
+                  active={window.location.pathname === "/register"}
+                >
+                  Sign Up
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
